f66b1a9c9659752dd1a33ce7e36279d7d06fe620
RayNjeri <njerirachael0@gmail.com>
2020-09-11 08:31:52 +0300

    Update state Fetch books from the json data(API) Update function to add books to collection
    Signed-off-by: RayNjeri <njerirachael0@gmail.com>

--- libraryrentalstore/src/containers/App.js
+++ libraryrentalstore/src/containers/App.js
@@ -1,4 +1,8 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { toast } from 'react-toastify';
+
+import { catalogue } from '../API/Catalogue.json';
+
 import {
   defaultRentDuration,
   defaultBookCount,
@@ -10,22 +14,33 @@ import {
 import '../index.css';
 
 
+let allBooks = [];
+Object.keys(catalogue).forEach(genre => {
+  const { books, rentCharge, minRentDuration, minimumCharge } = catalogue[genre];
+  books.forEach(book => {
+    book.rentCharge = rentCharge;
+    book.genre = genre;
+    if (minRentDuration) {
+      book.minRentDuration = minRentDuration;
+    }
+    if (minRentDuration) {
+      book.minimumCharge = minimumCharge;
+    }
+  });
+  allBooks = [...allBooks, ...books];
+});
+
+
 class App extends Component {
   state = {
     currentRead: '',
-    myCollection: {
-      Becoming: { rentDuration: 1, numOfBooks: 1 },
-      Atomic: { rentDuration: 1, numOfBooks: 2 },
-    },
-    library: [],
-    rentRate: 1,
+    myCollection: {},
   };
 
-  setBookState = ({ target: { value: currentRead } }) => {
-    this.setState({ currentRead });
-  };
+  setBookState = (event, { newValue }) =>
+    this.setState({ currentRead: newValue });
 
-  calculateTotalCharges= () => {
+  calculateTotalCharges = () => {
     const { myCollection, rentRate } = this.state;
     let totalCharge = 0;
     Object.keys(myCollection).forEach((title) => {
@@ -36,46 +51,59 @@ class App extends Component {
     return totalCharge;
   };
 
-  showMessage = msg => {};
+  showMessage = (msg, state) => {
+    switch (state) {
+      case 'error':
+        toast.error(msg);
+        break;
+      case 'warn':
+        toast.warn(msg);
+        break;
+      default:
+    }
+  };
 
-  addToCollection = event => {
+  addToCollection = (event) => {
     event.preventDefault();
-    const { currentRead, myCollection } = this.state;
 
+    const { currentRead, myCollection } = this.state;
     if (!currentRead) {
-      this.showMessage('Enter a valid title');
+      this.showMessage('Enter a valid book title', 'error');
       return;
     }
-
-    let newCollection = Object.assign({}, myCollection);
+    if (!allBooks.find((book) => book.title === currentRead)) {
+      this.showMessage('Sorry, The book is currently unavailable', 'error');
+      return;
+    }
+    const newCollection = Object.assign({}, myCollection);
 
     if (newCollection[currentRead]) {
       newCollection[currentRead].numOfBooks += 1;
     } else {
+      const bookDetails = allBooks.filter((x) => x.title === currentRead)[0];
       newCollection[currentRead] = {
+        ...bookDetails,
         rentDuration: defaultRentDuration,
         numOfBooks: defaultBookCount,
       };
+      this.setState({ myCollection: newCollection, currentRead: '' }, () =>
+        this.calculateCharge(currentRead)
+      );
     }
-
-    this.setState({ myCollection: newCollection, currentRead: '' });
-    this.calculateTotalCharges
-  ();
   };
+
   render() {
     const {
       addToCollection,
       setBookState,
       state,
-      calculateTotalCharges
-      ,
+      calculateTotalCharges,
     } = this;
     const { currentRead, myCollection } = state;
 
     const notEmptyCollection = Object.keys(myCollection).length > 0;
     const disableInputs = Object.keys(myCollection).length >= maxRentedBooks;
-    const totalCharge = calculateTotalCharges
-  ();
+    const totalCharge = calculateTotalCharges();
 
     return (
       <div className='container'>
@@ -114,4 +142,5 @@ class App extends Component {
     );
   }
 }
+
 export default App;
